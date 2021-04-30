2a18d7ea22810199a56173cba6c6a533974ff5eb
Ghinescu-Lucian <81215291+Ghinescu-Lucian@users.noreply.github.com>
2021-04-30 09:27:58 +0300

    Created database file for Login and Register

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.example.App;
 
 public class PrimaryController {
+    @FXML
+    private ImageView cartView;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        File cartFile =  new File("Images/cart.png");
+        Image cartImage = new Image("Images/cart.png");
+        cartView.setImage(cartImage);
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("secondary.fxml");
     }
 }
--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,12 +1,51 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.net.URL;
 
 public class SecondaryController {
+    @FXML
+    private ComboBox rol;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Text message;
+    @FXML
+    private ImageView lockView;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        rol.getItems().addAll("Client", "Vanzator");
+        File lockFile =  new File("Images/padlock_PNG9428.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockView.setImage(lockImage);
+    }
+
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) rol.getValue());
+            message.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            message.setText(e.getMessage());
+        }
+    }
+
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        App.setRoot("primary.fxml");
     }
 }
