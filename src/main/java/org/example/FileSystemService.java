039c92ca326552b25b93e25c66b5505e19183a5d
Georgevici-Giulia <81617512+Georgevici-Giulia@users.noreply.github.com>
2021-05-09 13:24:01 +0300

    created frontend home page for Cumparator

--- src/main/java/org/example/FileSystemService.java
+++ src/main/java/org/example/FileSystemService.java
@@ -5,8 +5,8 @@ import java.nio.file.Paths;
 
 public class FileSystemService {
     private static final String APPLICATION_FOLDER = "Aplicatie-cumparaturi";
-    private static final String USER_FOLDER = System.getProperty("C:\\Users\\ghine\\Desktop\\Facultate\\FIS\\AplicatieV2\\");
-    public static final Path APPLICATION_HOME_PATH = Paths.get("C:\\Users\\ghine\\Desktop\\Facultate\\FIS\\AplicatieV2\\Aplicatie-cumparaturi");
+    private static final String USER_FOLDER = System.getProperty("C:\\Users\\Giulia Olivera\\Desktop");
+    public static final Path APPLICATION_HOME_PATH = Paths.get("C:\\Users\\Giulia Olivera\\Desktop\\Aplicatie-cumparaturi");
 
     public static Path getPathToFile(String... path) {
         return APPLICATION_HOME_PATH.resolve(Paths.get(".", path));
