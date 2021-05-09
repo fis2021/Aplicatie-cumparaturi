e1260d3ff148d086d585e3ddafcec17f4c30aa69
Georgevici-Giulia <81617512+Georgevici-Giulia@users.noreply.github.com>
2021-05-09 14:00:29 +0300

    fix image error in home page client

--- src/main/java/org/example/HomePageClientController.java
+++ src/main/java/org/example/HomePageClientController.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class HomePageClientController {
+    @FXML
+    private ImageView fundal;
+    @FXML
+    public void initialize() {
+
+        File fundalFile =  new File("Images/fundal_client.jpg");
+        Image fundalImage = new Image(fundalFile.toURI().toString());
+        fundal.setImage(fundalImage);
+    }
 }
+
