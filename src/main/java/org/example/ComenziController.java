796b153975adc21de2995902a6a4c22492319ed6
Georgevici-Giulia <81617512+Georgevici-Giulia@users.noreply.github.com>
2021-05-10 17:59:36 +0300

    Added GUI for Comand tab

--- src/main/java/org/example/ComenziController.java
+++ src/main/java/org/example/ComenziController.java
@@ -9,4 +9,24 @@ public class ComenziController {
     public void SwitchToHome() throws IOException {
         App.setRoot("HomePageVanzator.fxml");
     }
+    @FXML
+    private void SwitchToEditareProduse() throws IOException {
+        App.setRoot("EditareProduse.fxml");
+    }
+
+    @FXML
+    private void SwitchToStergereProduse() throws IOException {
+        App.setRoot("StergereProduse.fxml");
+    }
+
+    @FXML
+    private void SwitchToAdaugareProduse() throws IOException {
+        App.setRoot("AdaugareProduse.fxml");
+    }
+
+    @FXML
+    private void SwitchToLogin() throws IOException{
+        App.setRoot("primary.fxml");
+        App.setUser(null);
+    }
 }
