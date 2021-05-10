472bd8cb99f185b7c2af3f02f283e299fd99f81b
Georgevici-Giulia <81617512+Georgevici-Giulia@users.noreply.github.com>
2021-05-10 14:12:05 +0300

    Added basic GUI pages for editing products

--- src/main/java/org/example/EditareProduseController.java
+++ src/main/java/org/example/EditareProduseController.java
@@ -9,4 +9,27 @@ public class EditareProduseController {
     public void SwitchToHome() throws IOException {
         App.setRoot("HomePageVanzator.fxml");
     }
-}
+        @FXML
+        private void SwitchToStergereProduse() throws IOException {
+            App.setRoot("StergereProduse.fxml");
+        }
+
+        @FXML
+        private void SwitchToAdaugareProduse() throws IOException {
+            App.setRoot("AdaugareProduse.fxml");
+        }
+
+        @FXML
+        private void SwitchToComenzi() throws IOException {
+            App.setRoot("Comenzi.fxml");
+        }
+
+        @FXML
+        private void SwitchToLogin() throws IOException{
+            App.setRoot("primary.fxml");
+
+        }
+    }
+
+
+
--- src/main/java/org/example/StergereProduseController.java
+++ src/main/java/org/example/StergereProduseController.java
@@ -9,4 +9,22 @@ public class StergereProduseController {
     public void SwitchToHome() throws IOException {
         App.setRoot("HomePageVanzator.fxml");
     }
+    @FXML
+    private void SwitchToEditareProduse() throws IOException {
+        App.setRoot("EditareProduse.fxml");
+    }
+    @FXML
+    private void SwitchToAdaugareProduse() throws IOException {
+        App.setRoot("AdaugareProduse.fxml");
+    }
+
+    @FXML
+    private void SwitchToComenzi() throws IOException {
+        App.setRoot("Comenzi.fxml");
+    }
+
+    @FXML
+    private void SwitchToLogin() throws IOException{
+        App.setRoot("primary.fxml");
+    }
 }
