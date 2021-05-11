0a80662365ae9c72600fa79fa3116724ff06b11e
Ghinescu-Lucian <81215291+Ghinescu-Lucian@users.noreply.github.com>
2021-05-11 17:08:00 +0300

    Fixed setCantitate and LogOut errors.

--- src/main/java/org/example/StergereProduseController.java
+++ src/main/java/org/example/StergereProduseController.java
@@ -56,6 +56,8 @@ public class StergereProduseController {
     @FXML
     private void SwitchToLogin() throws IOException{
         App.setRoot("primary.fxml");
+        UserService.updateUser(App.user);
+        App.setUser(null);
     }
 
     public void stergereProdus() {
--- src/main/java/org/example/VizProduseController.java
+++ src/main/java/org/example/VizProduseController.java
@@ -1,12 +1,102 @@
 package org.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class VizProduseController {
+
     @FXML
-    public void SwitchToHome() throws IOException {
-        App.setRoot("HomePageClient.fxml");
+        private TextField idProdus;
+    @FXML
+        private TextField cantitate;
+    @FXML
+        private Text mesaj;
+    @FXML
+        private Text total;
+    @FXML
+        private Text mesajCauta;
+    @FXML
+        private TextArea ListaProduse;
+    private static  ArrayList<User> vanzatori= UserService.getVanzatori();
+    private static  Produs p;
+
+    @FXML
+    private void initialize(){
+
+        String s="Id produs    Denumire    Cantitate Disponibila   Pret   Descriere \n";
+        for(User user: vanzatori){
+            s=s+" Produsele oferite de :"+user.getUsername()+"\n";
+            for(Produs prod:user.produse){
+                s=s+prod.getId()+"   "+prod.getDenumire()+"   "+prod.getCantitate()+"  "+prod.getPret()+" lei    " + "Descriere\n";
+            }
+            s=s+"\n";
+        }
+        ListaProduse.setText(s);
+    }
+
+    @FXML
+    private void SwitchToIstoric() throws IOException {
+        App.setRoot("IstoricComenzi.fxml");
+    }
+
+    @FXML
+    private void SwitchToCos() throws IOException {
+        App.setRoot("CosCumparaturi.fxml");
+    }
+
+    @FXML
+    private void SwitchToProduse() throws IOException {
+        App.setRoot("VizProduse.fxml");
+
+    }
+
+    @FXML
+    private void SwitchToLogin() throws IOException{
+        App.setRoot("primary.fxml");
+        App.setUser(null);
+        App.cosCumparaturi.clear();
+    }
+
+    public void adaugaInCos(ActionEvent actionEvent) {
+        int c;
+        if(p!=null) {
+            c = Integer.parseInt(cantitate.getText());
+            if (c <= p.getCantitate()) {
+                if (c != 0) {
+                    Produs q = new Produs(p.getPret(), c, p.getDescriere(), p.getDenumire());
+                    q.setId(p.getId());
+                    App.cosCumparaturi.add(q);
+                    mesaj.setText("Produs adaugat cu succes!");
+                }
+                else mesaj.setText("Adaugati o cantitate!");
+            }
+            else mesaj.setText("Cantitate indisponibila!");
+        }
+        else{
+            mesajCauta.setText("Introduceti id-ul unui produs!");
+        }
+
+    }
+
+    public void cautaProdus(ActionEvent actionEvent) {
+        p=null;
+        for(User user:vanzatori){
+            for(Produs prod:user.produse)
+                if(Objects.equals(idProdus.getText(),prod.getId())) {
+                    p = prod;
+                    mesajCauta.setText("Produs gasit!");
+                    break;
+                }
+        }
+        if(p==null){
+            mesajCauta.setText("Produs inexistent!");
+        }
     }
 }
