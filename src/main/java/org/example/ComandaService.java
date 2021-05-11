c39501f1a8a8177cdf6bdd51a81f6a89b428df28
Ghinescu-Lucian <81215291+Ghinescu-Lucian@users.noreply.github.com>
2021-05-11 14:59:25 +0300

    Added view and accept/decline orders functionality to Vendor.

---
+++ src/main/java/org/example/ComandaService.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+
+
+import javax.validation.constraints.Null;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static org.example.FileSystemService.getPathToFile;
+
+public class ComandaService {
+
+    private static ObjectRepository<Comanda> comandaRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("Order.db").toFile())
+                .openOrCreate("test", "test");
+        comandaRepository = database.getRepository(Comanda.class);
+    }
+
+    public static void addComanda(Comanda c){
+        comandaRepository.insert(c);
+    }
+
+    public static void updateComanda(Comanda c){
+        comandaRepository.update(c);
+    }
+
+    public static int lastComanda(User p){
+        String s="null";
+        String[] parti;
+        int x=0;
+        for(Comanda c:comandaRepository.find()){
+            if(Objects.equals(p.getUsername(),c.getClient())){
+                s=c.getId();
+            }
+        }
+        if(s!="null") {
+            parti = s.split("#");
+            x=Integer.parseInt(parti[1]);
+        }
+        return x;
+
+    }
+
+    public static ArrayList<Comanda> getComenziVanzator(User vanzator){
+        ArrayList<Comanda> comenzi= new ArrayList<Comanda>();
+        for(Comanda c:comandaRepository.find()){
+            if(Objects.equals(c.getVanzator().getUsername(),vanzator.getUsername()))
+            {
+                comenzi.add(c);
+            }
+        }
+        return comenzi;
+    }
+
+    public static Comanda getComanda(String id){
+        Comanda x=null;
+        for(Comanda c:comandaRepository.find()){
+            if(Objects.equals(id,c.getId()))
+                x=c;
+        }
+        return x;
+    }
+
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-512 does not exist!");
+        }
+        return md;
+    }
+
+
+}
--- src/main/java/org/example/ComenziController.java
+++ src/main/java/org/example/ComenziController.java
@@ -1,10 +1,35 @@
 package org.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class ComenziController {
+
+    @FXML
+        private TextField idComanda;
+    @FXML
+        private TextArea mesaj;
+    @FXML
+        private TextArea ListaComenzi;
+    @FXML
+        private Text mesaj1;
+    @FXML
+        private Text mesaj2;
+    @FXML
+        private RadioButton accept;
+    @FXML
+        private RadioButton decline;
+    @FXML
+        private ToggleGroup Raspuns;
+    private static Comanda c;
+
     @FXML
     public void SwitchToHome() throws IOException {
         App.setRoot("HomePageVanzator.fxml");
@@ -29,4 +54,28 @@ public class ComenziController {
         App.setRoot("primary.fxml");
         App.setUser(null);
     }
+
+    @FXML
+    public void cautaComanda() {
+        c=ComandaService.getComanda(idComanda.getText());
+        if(c==null)
+            mesaj2.setText("Comanda inexistenta!");
+        else mesaj2.setText("Comanda gasita!");
+    }
+
+    @FXML
+    public void salveazaComanda() {
+        boolean a;
+        if(c!= null){
+            c.setMesaj(mesaj.getText());
+            if(accept.isSelected())
+                a=true;
+            else a=false;
+            c.setAcceptare(a);
+            ComandaService.updateComanda(c);
+            mesaj1.setText("Comanda slavata cu succes!");
+        }
+    }
+
+
 }
