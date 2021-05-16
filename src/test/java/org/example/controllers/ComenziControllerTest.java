385348e73e2ef59cf307ffc8c7cd9c06317b10e9
Ghinescu-Lucian <81215291+Ghinescu-Lucian@users.noreply.github.com>
2021-05-16 10:56:45 +0300

    Added Jacoco plugin and tests for IstoricComenziController and ComenziController.

--- src/test/java/org/example/controllers/ComenziControllerTest.java
+++ src/test/java/org/example/controllers/ComenziControllerTest.java
@@ -2,6 +2,162 @@ package org.example.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.Assertions;
+import org.example.App;
+import org.example.exceptions.UsernameAlreadyExistsException;
+import org.example.models.Produs;
+import org.example.models.User;
+import org.example.services.ComandaService;
+import org.example.services.FileSystemService;
+import org.example.services.UserService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.io.IOException;
+import java.lang.IndexOutOfBoundsException;
+
+import static org.example.App.getCos;
+import static org.example.App.loadFXML;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+@ExtendWith(ApplicationExtension.class)
 class ComenziControllerTest {
 
+    private static Scene scene;
+    private static int i = 0;
+
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        FileSystemService.setApplicationFolder("AplicatieCumparaturi_test");
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath()
+                .toFile());
+        FileSystemService.initDirectory();
+    }
+
+    @AfterAll
+    static void afterAll()
+            throws IOException {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath()
+                .toFile());
+    }
+
+    @BeforeEach
+    void setUp() {
+        //App.golireCos();
+        UserService.removeUsers();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        App.golireCos();
+        UserService.removeUsers();
+        UserService.closeDatabase();
+        ComandaService.closeDatabase();
+    }
+
+    @Start
+    public void start(Stage stage) throws Exception {
+        //FileSystemService.initDirectory();
+
+        UserService.initDatabase();
+        ComandaService.initDatabase();
+        // System.out.println(UserService.getUser("c1").getUsername());
+        UserService.addUser("V1", "v1", "Vanzator");
+        App.setUser(UserService.getUser("V1"));
+        App.getUser().setProduseNull();
+        UserService.updateUser(App.getUser());
+        App.getUser().addProduse(new Produs(1.0, 200, "Mere romanesti ", "Mere Golden"));
+        UserService.updateUser(App.getUser());
+        App.getUser().addProduse(new Produs(2.0, 200, "Mere romanesti ", "Mere Ionatan"));
+        UserService.updateUser(App.getUser());
+        UserService.addUser("c1", "c1", "Client");
+        App.setUser(UserService.getUser("c1"));
+        if (i == 0) {
+            App.addCos(UserService.getUser("V1").getProduse().get(0));
+            App.addCos(UserService.getUser("V1").getProduse().get(1));
+            ComandaService.addComanda(App.getCos(), "Timisoara, Cantacuzino nr 1", "Ramburs", "-", 600.0, App.getUser(), UserService.getUser("V1"), "0727217128", "Astazi");
+            ComandaService.addComanda(App.getCos(), "Timisoara, Cantacuzino nr 2", "Ramburs", "-", 600.0, App.getUser(), UserService.getUser("V1"), "0727213128", "Maine");
+            i = 1;
+        }
+        App.setUser(UserService.getUser("V1"));
+
+        //  System.out.println(App.getCos());
+        scene = new Scene(loadFXML("Comenzi.fxml"), 950, 700);
+        stage.setTitle("Aplicatie Cumparaturi -test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    @DisplayName("Verify if OrdersHistory tab is displayed and initialize method works")
+    void testDisplayOrdersHistory(FxRobot robot) {
+
+    }
+    @Test
+    @DisplayName("Verify cautaComanda method - succesful scenario")
+    void testCautaComanda(FxRobot robot){
+        robot.clickOn("#idComanda").write("c1#0");
+        robot.clickOn("#cautaButton");
+        assertThat(robot.lookup("#mesaj2").queryText()).hasText("Comanda gasita!");
+
+    }
+
+    @Test
+    @DisplayName("Verify cautaComanda method - fail scenario")
+    void testCautaComanda2(FxRobot robot){
+        robot.clickOn("#idComanda").write("c1#2");
+        robot.clickOn("#cautaButton");
+        assertThat(robot.lookup("#mesaj2").queryText()).hasText("Comanda inexistenta!");
+
+    }
+
+    @Test
+    @DisplayName("Verify salveazaComanda method - successful/fail scenario")
+    void testSalveazaComanda(FxRobot robot){
+        //successful
+        robot.clickOn("#idComanda").write("c1#0");
+        robot.clickOn("#cautaButton");
+        assertThat(robot.lookup("#mesaj2").queryText()).hasText("Comanda gasita!");
+        robot.clickOn("#mesaj").write("Maine predam marfa la curier!");
+        robot.clickOn("#accept");
+        robot.clickOn("#salveazaButton");
+
+        assertThat(robot.lookup("#mesaj1").queryText()).hasText("Comanda salvata cu succes!");
+        assertThat(ComandaService.getAllComanda().get(0).getMesaj()).isEqualTo("Maine predam marfa la curier!");
+        assertThat(ComandaService.getAllComanda().get(0).getAcceptare()).isEqualTo("Acceptata");
+       // assertThat(ComandaService.getAllComanda().get(0))
+
+        //fail but at least success ;)
+        robot.clickOn("#idComanda").eraseText(4);
+        robot.clickOn("#idComanda").write("c1#1");
+        robot.clickOn("#mesaj").eraseText(29);
+        robot.clickOn("#mesaj").write("Mesaj de proba!");
+        robot.clickOn("#accept");
+        robot.clickOn("#salveazaButton");
+        robot.clickOn("#decline");
+        robot.clickOn("#salveazaButton");
+
+        assertThat(robot.lookup("#mesaj1").queryText()).hasText("Comanda salvata cu succes!");
+//        assertThat(ComandaService.getAllComanda().get(1).getMesaj()).isEqualTo("Produsul numai e pe stoc!");
+       // assertThat(ComandaService.getAllComanda().get(1).getAcceptare()).isEqualTo("Respinsa");
+
+    }
+    @Test
+    @DisplayName("Verify salveazaComanda method - a fail scenario")
+    void testSalveazaComanda2(FxRobot robot){
+        robot.clickOn("#idComanda").write("c1#0");
+        robot.clickOn("#cautaButton");
+        robot.clickOn("#decline");
+        robot.clickOn("#salveazaButton");
+
+        assertThat(robot.lookup("#mesaj2").queryText()).hasText("Completati mesajul!");
+    }
 }
